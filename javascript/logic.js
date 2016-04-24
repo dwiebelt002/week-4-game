938585ccff9718355661f071710aad51ef33cd6d
Devin <devinwiebelt@gmail.com>
2016-04-24 16:04:11 -0400

    second commit

---
+++ javascript/logic.js
@@ -0,0 +1,33 @@
+var userPlayer //represents user's character choice
+var computerPlayer //respresents computer character
+
+var playerAttack
+var computerAttack
+
+
+// Health Counters for both user and opponent
+var userHealth = 0
+var enemyHealth = 0
+
+//pre-set character attributes
+var player = function (health, strength, defense){
+this.health = health;
+this.strength = strength;
+this.defense = defense;
+};
+
+var opponent = function (health, strength, defense){
+this.health = health;
+this.strength = strength;
+this.defense = defense;
+};
+
+var nedStark = new opponent(400, 20, 12)
+var danyTarg = new oppenent(400, 25, 8)
+var jofBar = new opponent(300, 25, 15)
+var stanBar = new opponent(350, 20, 10)
+var tywinLan = new opponent(350, 15, 20)
+var balGrey = new opponent(300, 10, 25)
+
+
+//player selection
